4911eb061bc932f7cd0bddc64e804f274195c5a2
Tim Osborn <tim@memelab.com.au>
2018-04-15 22:17:17 +1000

    Configure gatsby-plugin-netlify to hide the site behind basic auth

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,18 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-netlify-cms',
+
+    // gatsby-plugin-netlify must be the last plugin
+    {
+      // https://www.gatsbyjs.org/packages/gatsby-plugin-netlify/
+      resolve: 'gatsby-plugin-netlify',
+      options: {
+        headers: {
+          "/*": [
+            "Basic-Auth: interplay:drafty-parcel-subject",
+          ],
+        },
+      },
+    },
   ],
 };
