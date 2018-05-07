5e5ce5d9dce5c6099348583d638046a0e69a7f40
Tim Osborn <tim@memelab.com.au>
2018-05-07 17:48:46 +1000

    Add header logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import './Header.css'
+import './interplay-logo.png'
 
 
 const colours = {
@@ -14,13 +15,11 @@ const colours = {
 
 const Header = () => (
   <nav className="Header">
-    <div>
-      <h1>
-        <Link to="/">
-          The Interplay Project
-        </Link>
-      </h1>
-    </div>
+    <h1>
+      <Link to="/">
+        <span>The Interplay Project</span>
+      </Link>
+    </h1>
 
     <ul className="Header__links">
       <li><Link to="/">link</Link></li>
