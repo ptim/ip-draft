315170d0f81a1c0dee0cf96e51a78e9f949c725c
Tim Osborn <tim@memelab.com.au>
2018-05-07 15:51:04 +1000

    Add homepage

---
+++ src/pages/homepage/index.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Header from '../../components/Header'
+
+import './homepage.css'
+import './ip-bg.jpg'
+
+
+const IndexPage = () => (
+  <div className="TemplateWrapper">
+    <Header />
+
+    <div className="Homepage">
+      <div className="slab hero">
+        <div className="container">
+          <p>Using data to change practice</p>
+          <p>Wellbeing through culture, collaboration and science</p>
+          <p>We help different groups work together to design and evaluate programs based on shared measures of success</p>
+        </div>
+      </div>
+
+      <div className="slab">
+        <div className="container">
+          <ul>
+            <li>Indigenous Evaluations</li>
+            <li>Monitoring and Evaluation Frameworks</li>
+            <li>Social Impact Assessment</li>
+            <li>"Whole of System" Measurement and Mapping</li>
+            <li>Holistic Wellbeing Assessments</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="slab">
+        <div className="container">
+          The Interplay Wellbeing Framework is a scientific mapping tool…
+        </div>
+      </div>
+
+      <div className="slab">
+        <div className="container">
+          Tell Us about your Evaluation Needs
+        </div>
+      </div>
+
+      <div className="slab">
+        <div className="container cols">
+          <div className="col--50">
+            INTERACTIVE (with descriptions)
+          </div>
+
+          <div className="col--50">
+            <ul>
+              <li>Working Together "Two Ways"</li>
+              <li>Holistic</li>
+              <li>Measuring Cultural Values</li>
+              <li>Bringing Together Stories and Numbers</li>
+              <li>Strength Based</li>
+              <li>Culture Brokerage</li>
+              <li>Scientifically Robust</li>
+              <li>Statistical Mapping</li>
+              <li>Flexible and Dynamic</li>
+              <li>Shared Measures of Success</li>
+              <li>Bespoke Designs</li>
+              <li>Collective Impact</li>
+              <li>Collaborative co-designs</li>
+              <li>Evidence Based Systems</li>
+              <li>Time Series Data</li>
+              <li>Policy Insights</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="slab">
+        <div className="container">
+          <h2>Back story</h2>
+
+          <p>Our framework and indicators were designed from the ground-up over 6 years in Aboriginal communities in remote Australia.</p>
+
+          <p>Developed as a collaboration between the Ninti One Foundation, the Australian Government’s Department of Prime Minister and Cabinet and Flinders University as part of the Cooperative Research Centre for Remote Economic Participation (CRC-REP)</p>
+        </div>
+      </div>
+    </div>
+
+    <footer className="footer">
+      <div className="container cols">
+        <div className="col--50">Footer</div>
+        <div className="col--25 col--50--narrow">Footer</div>
+        <div className="col--25 col--50--narrow">Footer</div>
+      </div>
+    </footer>
+  </div>
+)
+
+export default IndexPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div className="IndexPage">
     <ul>
+      <li><Link to="/homepage/">Homepage</Link></li>
       <li><Link to="/survey-vis/">Survey Visualisation</Link></li>
       <li><Link to="/blog/adding-markdown-pages">Adding Markdown pages</Link></li>
     </ul>
