9c54ddb2eb6cba29a29c3a03e3c8ef57b2ac3782
Tim Osborn <tim@memelab.com.au>
2018-05-14 23:45:53 +1000

    Refactor header to scss

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,9 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import './Header.css'
+import './Header.scss'
 import './interplay-logo.png'
 
-
-const colours = {
-  orange: '#f47721',
-  blue: '#008fb4',
-  green: '#82c341',
-  smoke: '#414142',
-  lightSmoke: '#cccccc',
-}
 
 const Header = () => (
   <nav className="Header">
