2d1312a4c5f0d7366d0cd2d412b82c747c0f34fb
Tim Osborn <tim@memelab.com.au>
2018-05-21 18:03:55 +1000

    Add sticky footer

--- src/layouts/DefaultTemplateWrapper.js
+++ src/layouts/DefaultTemplateWrapper.js
@@ -9,7 +9,9 @@ const DefaultTemplateWrapper = ({ children }) => (
   <div className="DefaultTemplateWrapper">
     <Header />
 
-    {children}
+    <div className="stickyBody">
+      {children}
+    </div>
 
     <Footer />
   </div>
