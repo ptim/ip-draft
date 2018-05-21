c9142378f9c31712d9a2f46385c25f357ccf2bda
Tim Osborn <tim@memelab.com.au>
2018-05-21 17:15:58 +1000

    Render homeapage at the root

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,5 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div className="IndexPage">
-    <ul>
-      <li><Link to="/homepage/">Homepage</Link></li>
-      <li><Link to="/survey-vis/">Survey Visualisation</Link></li>
-      <li><Link to="/blog/adding-markdown-pages">Adding Markdown pages</Link></li>
-    </ul>
-  </div>
-)
-
-export default IndexPage
+import Homepage from './homepage'
+export default Homepage
