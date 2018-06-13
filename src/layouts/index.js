c8fd7e2de50934962d95652af836580f9fc5860b
Tim Osborn <tim@memelab.com.au>
2018-06-13 21:37:57 +1000

    Add fonts: Mairy and Open Sans

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import './gatsby-normalize.scss'
-// import './layout.scss'
-// import './typography.scss'
+import './layout.scss'
+import './font-mairy.css'
+import './font-open-sans.css'
+import './typography.scss'
 
 
 const TemplateWrapper = ({ children }) => (
