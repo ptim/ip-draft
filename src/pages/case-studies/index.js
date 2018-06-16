1687276ef8755e794c94dda23a89e7a7af92515f
Tim Osborn <tim@memelab.com.au>
2018-06-16 16:10:50 +1000

    Raw content and images for /case-studies/

--- src/pages/case-studies/index.js
+++ src/pages/case-studies/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import FaPdf from 'react-icons/lib/fa/file-pdf-o';
 
 import DefaultTemplateWrapper from '../../layouts/DefaultTemplateWrapper'
+
+// I've ordered these by the number in the the pdf title:
+import pdfCulture from './InterplayPoster2_Culture_19Oct2016.pdf'
+import pdfLiteracy from './InterplayPoster3_Literacy_19Oct2016.pdf'
+import pdfEmployment from './InterplayPoster4_Empowerment_19Oct2016.pdf'
+import pdfHealth from './InterplayPoster5_HolisticHealth_19Oct2016.pdf'
+// import pdfNumeracy from './TODO.pdf'
+
+import imgCulture from './case-study-culture.png'
+import imgEmployment from './case-study-employment.png'
+import imgLiteracy from './case-study-literacy.png'
+import imgHealth from './case-study-health.png'
+import imgNumeracy from './case-study-numeracy.png'
+
 
 
 const CaseStudies = () => (
@@ -15,9 +30,89 @@ const CaseStudies = () => (
 
       <div className="slab">
         <div className="container">
-          <p>
-            TODO
-          </p>
+          <p>900 Aboriginal adults were surveyed from 4 different remote Aboriginal communities. Interrelationships were identified with structural equation modeling. </p>
+
+          <div className="case-study">
+            <h2>Case Study: Pathways to Education Success</h2>
+
+            <p>Better literacy outcomes from culture in school</p>
+
+            <ul>
+              <li>Education outcomes are much better for those who learn about their own culture at school, and learn Aboriginal literacy as a stepping stone to English literacy.</li>
+              <li>Culturally relevant curriculum and learning styles improves education outcomes.</li>
+            </ul>
+
+            <p>Find out more here and read the science publication here. </p>
+
+            <img src={imgLiteracy} width={600} alt="Literacy structural equation model" />
+            <a href={pdfLiteracy}><FaPdf /></a>
+          </div>
+
+          <div className="case-study">
+            <h2>Case Study: It all comes back to Culture</h2>
+
+            <p>Practising culture empowers and leads to greater wellbeing</p>
+
+            <ul>
+              <li>Practising culture builds empowerment and strengthens spirituality, both of which in turn improve wellbeing</li>
+              <li>Practising culture is the key to improving wellbeing for Aboriginal people in remote Australia</li>
+            </ul>
+
+            <img src={imgCulture} width={600} alt="Culture structural equation model" />
+            <a href={pdfCulture}><FaPdf /></a>
+          </div>
+
+          <div className="case-study">
+            <h2>Case Study: Mental Health Key for Wellbeing</h2>
+
+            <p>Good mental health underlies better health and health services</p>
+
+            <ul>
+              <li>Integrating services for physical and mental health problems improves wellbeing</li>
+              <li>Holistic approach to health services needed</li>
+            </ul>
+
+            <img src={imgHealth} width={600} alt="Health structural equation model" />
+            <a href={pdfHealth}><FaPdf /></a>
+          </div>
+
+          <div className="case-study">
+            <h2>Case Study: Empowered to Study and Work</h2>
+
+            <p>Empowerment is the basis for better education and work outcomes</p>
+
+            <ul>
+              <li>Successful pathways between education and employment depend on their capacity to build empowerment.</li>
+              <li>Policy and practice for education and employment that focuses on building empowerment underlies optimal wellbeing.</li>
+              <li>That is, the more they empower people the more likely they are to succeed.</li>
+            </ul>
+
+            <p>
+              Find out more{' '}
+              <a href="http://box5182.temp.domains/~interri2/wp-content/uploads/2017/11/InterplayPoster3_Literacy_19Oct2016.pdf">here</a>{' '}
+              and read the science publication{' '}
+              <a href="https://link.springer.com/article/10.1007%252Fs10671-017-9217-z&amp;sa=D&amp;ust=1529130503901000">here</a>.
+            </p>
+
+            <img src={imgEmployment} width={600} alt="Employment structural equation model" />
+            <a href={pdfEmployment}><FaPdf /></a>
+          </div>
+
+          <div className="case-study">
+            <h2>Case Study: Numbers and Forms opens doors</h2>
+
+            <p>Confidence with paperwork opens pathways to success</p>
+
+            <p>60 Aboriginal adults were surveyed who had used a financial literacy program. Interrelationships were identified with structural equation modeling. </p>
+
+            <ul>
+              <li>Confidence with numbers and filling out forms is a pivotal step to improving work outcomes, money knowledge, social and emotional health and wellbeing. </li>
+              <li>Empowerment is a key foundation upon which pathways to wellbeing are built.</li>
+            </ul>
+
+            <img src={imgNumeracy} width={600} alt="Numeracy structural equation model" />
+            {/* <a href={pdfNumeracy}><FaPdf /></a> */}
+          </div>
 
         </div>
       </div>
