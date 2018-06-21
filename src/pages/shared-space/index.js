5828bbbe398527ddbf07b6a1be9eed112677f396
Tim Osborn <tim@memelab.com.au>
2018-06-22 00:38:42 +1000

    Stub mapping, shared-space pages

---
+++ src/pages/shared-space/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import DefaultTemplateWrapper from '../../layouts/DefaultTemplateWrapper'
+
+
+const SharedSpace = () => (
+  <DefaultTemplateWrapper>
+    <div className="SharedSpace">
+
+      <div className="slab">
+        <div className="container">
+          <h1 className="underline">SharedSpace</h1>
+
+          <p>
+            TODO
+          </p>
+
+        </div>
+      </div>
+
+    </div>
+  </DefaultTemplateWrapper>
+)
+
+export default SharedSpace
